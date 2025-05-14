b234cafc04467b504baad7ac2757fb1007b7e5b4
Shushar1k <145340194+Shushar1k@users.noreply.github.com>
2025-05-14 07:37:07 +0300

    Update lib.rs

--- udos/combinations/src/lib.rs
+++ udos/combinations/src/lib.rs
@@ -1,6 +1,19 @@
 #![forbid(unsafe_code)]
 
 pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let quantity = arr.len();
+    if k > quantity {
+        return vec![];
+    }
+    if k == 0 {
+        return vec![vec![]];
+    }
+    let mut result = combinations(&arr[1..], k - 1);
+    
+    for comb in &mut result {
+        comb.insert(0, arr[0]);
+    }
+    let second_part = combinations(&arr[1..], k);
+    result.extend(second_part);
+    return result;
 }
