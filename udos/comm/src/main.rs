2407cb6f3dac9d5ffe48e2cab74548f8a4e6941f
Shushar1k <145340194+Shushar1k@users.noreply.github.com>
2025-05-14 14:02:19 +0300

    Update main.rs

--- udos/comm/src/main.rs
+++ udos/comm/src/main.rs
@@ -1,8 +1,28 @@
 #![forbid(unsafe_code)]
 
-// TODO: your code goes here.
+use std::collections::HashSet;
+use std::{fs::File, io::BufRead, io::BufReader};
 
 fn main() {
-    // TODO: your code goes here.
-    unimplemented!()
+    let args = std::env::args().collect::<Vec<String>>();
+    let file_1 = File::open(&args[1]).unwrap();
+    let reader_1 = BufReader::new(file_1);
+    let data_1: HashSet<String> = reader_1.lines().collect();
+    
+    let file_2 = File::open(&args[2]).unwrap();
+    let reader_2 = BufReader::new(file_2);
+    let data_2: HashSet<String> = reader_2.lines().collect();
+    let mut result: HashSet<String> = HashSet::new();
+
+    for i in data_1 {
+        for j in data_2 {
+            if i == j {
+                result.insert(i);
+            }
+        }
+    }
+
+    for i in result {
+        println!("{}", i);
+    }
 }
